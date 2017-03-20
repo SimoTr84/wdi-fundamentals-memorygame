26c74543670410449991511f74af25593f052c8b
Simone <simone.trucco@gmail.com>
2017-03-21 09:37:36 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
-console.log ("up and running!");
+var cards = ["queen", "queen", "king", "king"];
 
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!!");
+	} else {
+	alert("Sorry, try again...");
+	}
+}
+
+/*
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
@@ -7,3 +27,4 @@ var cardFour = "king";
 
 console.log("User flipped " + cardOne);
 console.log("user flipped " + cardThree);
+*/
