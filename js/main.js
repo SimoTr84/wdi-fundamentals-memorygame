f14be5d05e761f5ba609ef0033bce8b2285aee4e
Simone <simone.trucco@gmail.com>
2017-03-22 16:59:52 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,23 +2,26 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!!");
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	} else {
-	alert("Sorry, try again...");
+		console.log("Sorry, try again!");
 	}
 }
 
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId])
+	if (cardsInPlay.length === 2) {					// unit 8
+	checkForMatch(0, 2);
+	}	
+
+}
+	//cardsInPlay.push(cardId);
+flipCard(0);
+flipCard(2);
+
+// eliminated code
 /*
 var cardOne = "queen";
 var cardTwo = "queen";
@@ -27,4 +30,13 @@ var cardFour = "king";
 
 console.log("User flipped " + cardOne);
 console.log("user flipped " + cardThree);
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
 */
